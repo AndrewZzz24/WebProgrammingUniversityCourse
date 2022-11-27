c9b514efe6fa44b097efdc8e36d9fa77199cb3ed
Andrew Zmushko <70968410+AndrewZzz24@users.noreply.github.com>
2022-11-28 00:09:54 +0300

    fix: page navigation highlighter fixes

--- Sources/Scripts/activeButton.js
+++ Sources/Scripts/activeButton.js
@@ -1,7 +1,9 @@
 (function () {
     function pageIsActive() {
         let path = document.location.pathname.split('/').pop();
-        document.getElementById(path.substring(0, path.length - 5)).style.color = '#daa512';
+        let end =  path.substring(path.length - 5, path.length);
+        let page = end !== ".html" ? "index" : path.substring(0, path.length - 5);
+        document.getElementById(page).style.color = '#daa512';
     }
 
     document.addEventListener('DOMContentLoaded', () => {
