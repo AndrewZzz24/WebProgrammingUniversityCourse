67b8058e82eed4259fc443a7134233a5451fdeb0
Andrew Zmushko <70968410+AndrewZzz24@users.noreply.github.com>
2022-11-27 20:51:06 +0300

    fix: activeButton.js hot fixes

--- Sources/Scripts/activeButton.js
+++ Sources/Scripts/activeButton.js
@@ -1,13 +1,7 @@
 (function () {
-    let pages = {
-        'index.html': 'index',
-        'experience.html': 'experience',
-        'projects.html': 'projects',
-    };
-
     function pageIsActive() {
-        let itemId = pages[document.location.pathname.split('/').pop()];
-        document.getElementById(itemId).style.color = '#daa512';
+        let path = document.location.pathname.split('/').pop();
+        document.getElementById(path.substring(0, path.length - 5)).style.color = '#daa512';
     }
 
     document.addEventListener('DOMContentLoaded', () => {
