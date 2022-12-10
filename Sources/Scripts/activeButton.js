acd470b8aeeb1f06376d89b70dba6bef28840f1a
Andrew Zmushko <70968410+AndrewZzz24@users.noreply.github.com>
2022-12-10 20:18:43 +0300

    fix: fix active button

--- Sources/Scripts/activeButton.js
+++ Sources/Scripts/activeButton.js
@@ -1,9 +1,13 @@
 (function () {
     function pageIsActive() {
-        let path = document.location.pathname.split('/').pop();
-        let end =  path.substring(path.length - 5, path.length);
-        let page = end !== ".html" ? "index" : path.substring(0, path.length - 5);
-        document.getElementById(page).style.color = '#daa512';
+        let links = document.querySelectorAll(".default-header nav a")
+        console.log(links)
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].pathname === document.location.pathname) {
+                document.getElementById(links[i].id).style.color = '#daa512';
+                break
+            }
+        }
     }
 
     document.addEventListener('DOMContentLoaded', () => {
