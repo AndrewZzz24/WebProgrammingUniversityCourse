05764673d57e92e6f57c9533d3c4a22e1e70ab54
Andrew Zmushko <70968410+AndrewZzz24@users.noreply.github.com>
2022-12-10 18:40:28 +0300

    fix: feedbackForm script issues fixed

--- Sources/Scripts/feedbackForm.js
+++ Sources/Scripts/feedbackForm.js
@@ -1,47 +1,46 @@
 function listenToSubmit() {
     document.addEventListener("submit", (event) => {
         event.preventDefault()
-        const title = document.getElementById("title").value
-        const contact = document.getElementById("contact").value
-        const description = document.getElementById("description").value
-        if (title.length === 0 || description.length === 0 || contact.length === 0) {
-            alert("Все поля должны быть заполнены.")
-            return
-        }
-        document.getElementById("title").value = "";
-        document.getElementById("contact").value = "";
-        document.getElementById("description").value = "";
-        const hash = new Date().getTime() + title
+
+        const title = document.getElementById("title").value.trim()
+        const contact = document.getElementById("contact").value.trim()
+        const description = document.getElementById("description").value.trim()
         const suggestObject = {"title": title, "contact": contact, "description": description}
-        localStorage.setItem(hash, JSON.stringify(suggestObject))
+
+        document.getElementById("feedback-form-id").reset();
+
+        if (localStorage.length === 0) {
+            localStorage.setItem("feedbackStorage", '[]')
+        }
+
+        let feedback_storage = JSON.parse(localStorage.getItem("feedbackStorage"))
+        feedback_storage.push(JSON.stringify(suggestObject))
+        localStorage.setItem("feedbackStorage", JSON.stringify(feedback_storage))
+
         renderSuggest(suggestObject)
     })
 }
 
 function displayMetrics() {
-    const keys = Object.keys(localStorage)
-    let count_of_suggest = keys.length
-    while (count_of_suggest--) {
-        renderSuggest(JSON.parse(localStorage.getItem(keys[count_of_suggest])))
+    let feedback_storage = JSON.parse(localStorage.getItem("feedbackStorage"))
+    if (feedback_storage === null) return
+    for (let i = 0; i < feedback_storage.length; i++) {
+        renderSuggest(JSON.parse(feedback_storage[i]))
     }
 }
 
 function renderSuggest(suggestObject) {
-    console.log(suggestObject)
-    const rowRender = `
-      <tr>
-          <td>
-              ${suggestObject["title"].toString()}
-          </td>
-          <td>
-              ${suggestObject["contact"].toString()}
-          </td>
-          <td>
-              ${suggestObject["description"].toString()}
-          </td>
-      </tr>
-    `;
-    document.querySelector(".grid-from-table-body").insertAdjacentHTML("beforeend", rowRender);
+    let tbody = document.querySelector(".grid-from-table-body");
+    let template = document.querySelector('#feedback-form-response');
+
+    let clone = template.content.cloneNode(true);
+    let td = clone.querySelectorAll("td");
+
+    td[0].textContent = suggestObject["title"];
+    td[1].textContent = suggestObject["contact"];
+    td[2].textContent = suggestObject["description"];
+
+    tbody.appendChild(clone);
 }
 
 (function () {
--- Sources/Scripts/loadTimer.js
+++ Sources/Scripts/loadTimer.js
@@ -1,9 +1,10 @@
 (function () {
-    let open_time = window.performance.timing.domLoading - window.performance.timing.navigationStart;
+    let open_time = window.performance.timing.loadEventStart - window.performance.timing.navigationStart;
 
     function load_time() {
         document.getElementById('load-time').innerHTML =
             'Время загузки составило: ' + open_time / 1000 + ' сек';
     }
+
     document.addEventListener('DOMContentLoaded', load_time);
 })();
